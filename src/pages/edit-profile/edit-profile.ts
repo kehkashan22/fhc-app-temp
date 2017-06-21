462c9e055dca6c1b66fbc0207acdf51c2a8d2426
shantanu_kamdi <shantanu_kamdi@persistent.com>
2017-06-21 08:19:55 +0530

    Fetched user profile for editing

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, ViewController, LoadingController } from 'ionic-angular';
+/* Forms module */
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-/**
- * Generated class for the EditProfilePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { AuthProvider } from '../../providers/auth';
+
+import firebase from 'firebase';
+
 @IonicPage()
 @Component({
   selector: 'page-edit-profile',
@@ -14,11 +14,95 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EditProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private user;
+  private db;
+  /* FormGroup which will be used in html */
+  private form: FormGroup;
+ 
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private modalCtrl: ModalController,
+              private viewCtrl: ViewController,
+              private _auth: AuthProvider,
+              private formBuilder: FormBuilder,
+              private loadingCtrl: LoadingController
+  ) {
+    this.form = formBuilder.group({
+        fullName: [ '', Validators.required],
+        emailId: ['', Validators.required],
+        phoneNumber: [ '', Validators.compose([Validators.required, Validators.minLength(10)])],
+        password: [ '', Validators.required],
+        address: [''],
+        attemptNo: ['', Validators.required],
+        pincode: [ '', Validators.minLength(6)],
+        attemptDate: ['', Validators.required],
+        dob: [''],
+        gender: [''],
+        typeOfCourse: ['', Validators.required]
+    });
+    
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditProfilePage');
+
+    
+      /* Loader */
+    let loader = this.loadingCtrl.create({
+      spinner: "bubbles",
+      content: 'Loading...'
+    });
+    loader.present();
+
+
+    this.getUserProfile().then(data => {
+      this.user = data;
+      
+      console.log("data", this.user.newUserId);
+      let user = {
+        fullName: this.user.newUserId.fullName,
+        emailId: this.user.newUserId.emailId,
+        phoneNumber: this.user.newUserId.phoneNumber,
+        address: this.user.newUserId.address,
+        attemptNo: this.user.newUserId.attemptNo,
+        pincode: this.user.newUserId.pincode,
+        attemptDate: this.user.newUserId.attemptDate,
+        dob: this.user.newUserId.dob,
+        gender: this.user.newUserId.gender,
+        typeOfCourse: this.user.newUserId.typeOfCourse
+      }
+      this.form.get('fullName').setValue(user.fullName);
+      this.form.get('emailId').setValue(user.emailId);
+      this.form.get('phoneNumber').setValue(user.phoneNumber);
+      this.form.get('address').setValue(user.address);
+      this.form.get('attemptNo').setValue(user.attemptNo);
+      this.form.get('attemptDate').setValue(user.attemptDate);
+      this.form.get('pincode').setValue(user.pincode);
+      this.form.get('dob').setValue(user.dob);
+      this.form.get('gender').setValue(user.gender);
+      this.form.get('typeOfCourse').setValue(user.typeOfCourse);
+
+      loader.dismiss();
+    });
   }
 
+  closeModal(){
+    this.viewCtrl.dismiss();
+  }
+
+  update(){
+
+  }
+
+  getUserProfile(): Promise<any>{
+    return new Promise((resolve, reject) => {
+      firebase.database().ref('/users')
+      .child(firebase.auth().currentUser.uid)
+      .on('value', data => {
+        console.log(data);
+        resolve(data.val());
+      })
+    });
+  }
 }
